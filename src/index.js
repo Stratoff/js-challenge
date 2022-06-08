f9abf41ccf690b3d499c72f7d61e9c53464b2fe2
Javier Garcia <jegn28@gmail.com>
2022-06-07 21:25:54 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -17,35 +17,35 @@ const handleOffset = () => {
 
 const getData = async api => {
 
-  try {
-    const pagination = handleOffset();
-    const resp = await fetch(api + `/?limit=${LIMIT}&offset=${pagination}`);
-    const products = await resp.json();
-    const output = await products.map(product => `
-      <article class="Card">
-        <img src="${product.images[0]}" alt="${product.title}" />
-        <h2>
-          ${product.title}
-          <small>$ ${product.price}</small>
-        </h2>
-      </article>
-    `);
-    let newItem = document.createElement('section');
-    newItem.classList.add('Items');
-    output.forEach(product => {
-      newItem.innerHTML += product;
-    });
-    $app.appendChild(newItem);
-
-    if (pagination === 195 || output.length < 10) {
-      const msgNoMoreProducts = document.createElement('section');
-      msgNoMoreProducts.innerHTML = "<h3 style='text-align:center;'>Todos los productos Obtenidos</h3>";
-      document.getElementsByClassName("Main")[0].appendChild(msgNoMoreProducts);
-      intersectionObserver.unobserve($observe);
+  if (pagination <= 200) {
+    try {
+      const pagination = handleOffset();
+      const resp = await fetch(api + `/?limit=${LIMIT}&offset=${pagination}`);
+      const products = await resp.json();
+      const output = await products.map(product => `
+        <article class="Card">
+          <img src="${product.images[0]}" alt="${product.title}" />
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+      `);
+      let newItem = document.createElement('section');
+      newItem.classList.add('Items');
+      output.forEach(product => {
+        newItem.innerHTML += product;
+      });
+      $app.appendChild(newItem);
+    } catch (err) {
+      console.log(err)
     }
-
-  } catch (err) {
-    console.log(err)
+  } else {
+    const msgNoMoreProducts = document.createElement('section');
+    msgNoMoreProducts.innerHTML = "<h3 style='text-align:center;'>Todos los productos Obtenidos</h3>";
+    document.getElementsByClassName("Main")[0].appendChild(msgNoMoreProducts);
+    intersectionObserver.unobserve($observe);
+    $observe.remove();
   }
   
 }
@@ -55,11 +55,7 @@ const loadData = async () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      loadData();
-    }
-  });
+  if (entries[0].isIntersecting) loadData();
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
